6617fa9a41650342dba186f5254fd9fbe68d9df0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-10-26 09:02:40 +0300

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import AdminLayoutView from "../views/admin/AdminLayoutView.vue";
 import {onAuthStateChanged} from "firebase/auth";
 import {useFirebaseAuth} from "vuefire";
 
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [

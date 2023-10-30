ce40e3289ff3427ea2682cc12402bfaccc23bb9d
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-10-30 11:28:09 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,17 @@
 <script setup>
+import useProperties from "../composables/useProperties";
+import Property from "../components/Property.vue";
 
+
+const { propertiesCollection } = useProperties();
 </script>
 
 <template>
   <h1 class="text-center">Vue Real Estates</h1>
+
+  <v-card flat class="py-10">
+    <V-row>
+      <Property v-for="property in propertiesCollection" :key="property.id" :property="property" />
+    </V-row>
+  </v-card>
 </template>

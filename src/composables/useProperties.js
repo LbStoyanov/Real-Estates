b753584c8db44156679aa3fdfbc55702a5b915ce
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-10-30 14:19:24 +0200

    Update useProperties.js

--- src/composables/useProperties.js
+++ src/composables/useProperties.js
@@ -1,22 +1,11 @@
-import {collection, query, where, getDocs} from "firebase/firestore";
+import {collection} from "firebase/firestore";
 import {useFirestore, useCollection} from "vuefire";
-import {computed} from "vue";
 
 export default function useProperties() {
     const db = useFirestore();
-
     const propertiesCollection = useCollection(collection(db, "properties"));
-
-    const propertyPrice = computed(() => {
-        return (price) =>
-            Number(price).toLocaleString("en-US", {
-                style: "currency",
-                currency: "EUR",
-            });
-    });
 
     return {
         propertiesCollection,
-        propertyPrice,
     };
 }

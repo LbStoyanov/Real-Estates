7b582d400e46b44ddc29f6882de65e429b6d0eaf
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-10-30 13:03:35 +0200

    Update Property.vue

---
+++ src/components/Property.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  property: {
+    type: Object,
+  },
+  price: { type: Function },
+});
+</script>
+
+<template>
+  <v-col cols="12" md="4">
+    <v-card class="ml-5 mr-5">
+      <v-img :src="property.image" />
+      <v-card-title class="font-weight-bold">{{ property.title }}</v-card-title>
+      <v-card-text class="text-truncate">{{
+        property.description
+      }}</v-card-text>
+      <v-card-text class="text-h5 font-weight-bold"
+        >Price: {{ price(property.price) }}</v-card-text
+      >
+      <template v-slot:actions>
+        <v-btn block color="info" variant="tonal">See Details</v-btn>
+      </template>
+    </v-card>
+  </v-col>
+</template>

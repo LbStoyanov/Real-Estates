27d32cc4ecedce784143a267667a4985d97dbc09
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-10-31 14:23:11 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,32 @@
 <script setup>
 import useProperties from "../composables/useProperties";
 import Property from "../components/Property.vue";
-import {propertyPrice} from '../helpers'
+import { propertyPrice } from "../helpers";
 
-
-const { propertiesCollection } = useProperties();
+const { filteredProperties, swimmingPool, garden, airConditioning, internet } =
+  useProperties();
 </script>
 
 <template>
   <h1 class="text-center">Vue Real Estates</h1>
 
-  <v-card flat class="py-10" >
+  <v-card flat class="py-10">
+    <v-card-title class="text-h5">
+      Filter
+      <v-row>
+        <v-checkbox label="Swimming pool" v-model="swimmingPool" />
+        <v-checkbox label="Garden" v-model="garden" />
+        <v-checkbox label="Air Conditioning" v-model="airConditioning" />
+        <v-checkbox label="Internet" v-model="internet" />
+      </v-row>
+    </v-card-title>
     <V-row>
-      <Property v-for="property in propertiesCollection" :key="property.id" :property="property" :price="propertyPrice"/>
+      <Property
+        v-for="property in filteredProperties"
+        :key="property.id"
+        :property="property"
+        :price="propertyPrice"
+      />
     </V-row>
   </v-card>
 </template>

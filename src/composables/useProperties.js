5f88731f3708fe5a618a3b5040edb7b540596e87
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-10-31 14:22:51 +0200

    Update useProperties.js

--- src/composables/useProperties.js
+++ src/composables/useProperties.js
@@ -1,11 +1,44 @@
 import {collection} from "firebase/firestore";
 import {useFirestore, useCollection} from "vuefire";
+import {computed, ref} from "vue";
 
 export default function useProperties() {
+    const swimmingPool = ref(false);
+    const garden = ref(false);
+    const airConditioning = ref(false);
+    const internet = ref(false);
+
     const db = useFirestore();
     const propertiesCollection = useCollection(collection(db, "properties"));
 
+    const filteredProperties = computed(() => {
+        return propertiesCollection.value.filter((property) => {
+            if (swimmingPool.value && !property.swimmingPool) {
+                return false;
+            }
+
+            if (garden.value && !property.garden) {
+                return false;
+            }
+
+            if (airConditioning.value && !property.airConditioning) {
+                return false;
+            }
+
+            if (internet.value && !property.internet) {
+                return false;
+            }
+
+            return true;
+        });
+    });
+
     return {
         propertiesCollection,
+        filteredProperties,
+        swimmingPool,
+        garden,
+        airConditioning,
+        internet,
     };
 }

5d18874287343355f22bf607f8cac0a74b28cabe
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2023-10-31 13:58:55 +0200

    Update PropertyView.vue

--- src/views/PropertyView.vue
+++ src/views/PropertyView.vue
@@ -1,13 +1,23 @@
 <script setup>
+import { watch } from "vue";
 import { useRoute } from "vue-router";
 import { doc } from "firebase/firestore";
 import { useDocument, useFirestore } from "vuefire";
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LMarker, LPopup } from "@vue-leaflet/vue-leaflet";
+import useLocationMap from "../composables/useLocationMap";
 import { propertyPrice } from "../helpers";
+
+const { zoom, center } = useLocationMap();
 
 const route = useRoute();
 const db = useFirestore();
 const docRef = doc(db, "properties", route.params.id);
 const property = useDocument(docRef);
+
+watch(property, (property) => {
+  center.value = property.location;
+});
 </script>
 
 <template>
@@ -17,7 +27,9 @@ const property = useDocument(docRef);
     </v-card-title>
     <v-img :src="property?.image" height="450"></v-img>
 
-    <div class="bg-blue-grey-lighten-4 d-flex flex-column flex-md-row">
+    <div
+      class="bg-blue-grey-lighten-4 d-flex flex-column flex-md-row align-center"
+    >
       <v-card-text>
         Price:
         <span class="font-weight-bold">{{
@@ -42,8 +54,21 @@ const property = useDocument(docRef);
         <div class="text-pre-wrap py-10 mx-5">{{ property?.description }}</div>
       </v-col>
       <v-col cols="12" md="4">
-        <div class="py-10">
-          <p>Map</p>
+        <div class="py-10" style="height: 600px">
+          <LMap
+            v-model:zoom="zoom"
+            :center="center"
+            :use-global-leaflet="false"
+          >
+            <LMarker :lat-lng="center">
+              <LPopup>
+                {{ property.title }}
+              </LPopup>
+            </LMarker>
+            <LTileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            ></LTileLayer>
+          </LMap>
         </div>
       </v-col>
     </v-row>
